72622ff963afcb04fc58264d0924bd58920c3b66
Tom Brierley <brierleytom@gmail.com>
2020-02-08 10:52:44 +0000

    NEW: Fetch content from Guardian data API

--- src/containers/TabbedContent.js
+++ src/containers/TabbedContent.js
@@ -1,13 +1,68 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
 import Layout from '../components/Layout';
 
-const Calendar = () => {
+import { searchContent } from '../utils/api';
+
+const TabbedContent = () => {
+  // This could quite easily sit within Redux or Apollo but defining it as component state here
+  const [tabContent, setTabContent] = useState({
+    isFetching: false,
+    content: null,
+    error: false,
+    errorMessage: null,
+  });
+
+  const [activeTab, setActiveTab] = useState('uk-news');
+
+  const handleFetching = () => {
+    setTabContent({
+      isFetching: true,
+      content: null,
+      error: false,
+      errorMessage: null,
+    });
+  };
+
+  const handleFetchSuccess = data => {
+    setTabContent({
+      isFetching: false,
+      content: data,
+      error: false,
+      errorMessage: null,
+    });
+  };
+
+  const handleFetchError = () => {
+    setTabContent({
+      isFetching: false,
+      content: null,
+      error: true,
+      errorMessage:
+        'There has been a problem fetching content, please try again later/', // TODO: Parse this message from the API response
+    });
+  };
+
+  // Fetch content from API
+  const fetchContent = options => {
+    searchContent({
+      onFetching: handleFetching,
+      onSuccess: handleFetchSuccess,
+      onError: handleFetchError,
+      ...options,
+    });
+  };
+
+  // Get Initial Content - Renders only on mount
+  // TODO: Update eslint config so I don't have to disable that line below
+  useEffect(() => {
+    fetchContent({ sectionId: activeTab });
+  }, []); // eslint-disable-line
+
   return (
     <Layout title="Tabbed Content Example">
-      <p>Tabbed content coming soon</p>
+      {tabContent && JSON.stringify(tabContent)}
     </Layout>
   );
 };
 
-export default Calendar;
+export default TabbedContent;
---
+++ src/utils/api.js
@@ -0,0 +1,27 @@
+// Fetcher function for the Guardian Content API, handles callbacks and add API key
+export const fetchFromAPI = async (url, { onFetching, onSuccess, onError }) => {
+  // Call on fetching callback
+  if (onFetching) onFetching();
+
+  // Try to fetch data from the PI
+  try {
+    const response = await fetch(
+      `${url}&api-key=${process.env.REACT_APP_CONTENT_API_KEY}`,
+    );
+    const data = await response.json();
+
+    if (onSuccess) onSuccess(data?.response?.results || null);
+  } catch (error) {
+    // Handle the error
+    if (onError) onError(error);
+  }
+};
+
+// Searches content from the Guardian Content API using a sectionId
+export const searchContent = ({ sectionId, ...options }) => {
+  // Fetch API data
+  fetchFromAPI(
+    `${process.env.REACT_APP_CONTENT_API_URL}search?section=${sectionId}`,
+    options,
+  );
+};
